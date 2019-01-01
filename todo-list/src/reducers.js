7218b12bee3029401be498ae1e42bb1af404f02f
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-01 15:28:33 -0700

    write reducer to understand actions defined earlier. standard and ES6 syntax

--- todo-list/src/reducers.js
+++ todo-list/src/reducers.js
@@ -1,14 +1,21 @@
-//designing state shape
-{
-  VisibilityFilters: 'SHOW_ALL',
-  todos: [
-    {
-      text: 'Consider using Redux',
-      completed: true
-    },
-    {
-      text: 'Keep all state in a single tree',
-      completed: false
-    }
-  ]
+import { VisibilityFilters } from './actions'
+
+const initialState = {
+  VisibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: []
+}
+
+function todoApp(state, action) {
+  if (typeof state === 'undefined') {
+    return initialState
+  }
+
+  //for now don't handle actions just return given state
+  return state
+}
+
+//ES6 syntax
+function todoApp(state = initialState, action) {
+  //for now don't handle actions just return given state
+  return state
 }
