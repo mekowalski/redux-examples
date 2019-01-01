78b82e51f95804d5164587fcd53e1266393dc195
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-01 14:41:13 -0700

    create 2 actions

--- todo-list/src/action.js
+++ todo-list/src/action.js
@@ -5,3 +5,17 @@ const ADD_TODO = 'ADD_TODO'
   type: ADD_TODO.
   text: 'Build my first Redux App'
 }
+
+//add another action to complete todo item
+//refer to specific todo item by INDEX
+//in real app it's wiser to generate unique ID every time a new item is created
+{
+  type: TOGGLE_TODO,
+  index: 5
+}
+
+//add another action to change current visible todos
+{
+  type: SET_VISIBILTY_FILTER.
+  fitler: SHOW_COMPLETED
+}
