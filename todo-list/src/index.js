9526c3d9c0ba5fcd502321a5052958778e57fb77
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-02 14:41:39 -0700

    install react-redux and redux, running into issure on browser

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Create store while having Reducer(combineReducers()) and pass into createStore()
+import { createStore } from 'redux'
+import todoApp from './reducers'
+
+const store = createStore(todoApp)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
